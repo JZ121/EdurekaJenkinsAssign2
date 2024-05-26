b7bf41b3b286c63c42d1f5e824d62167ca6e9c3e
JZ121 <jafar_jz@hotmail.com>
2024-05-26 18:33:35 +0530

    second commit with tomcat deploy

--- src/main/java/com/example/demo/EdurekaJenkinsAssign2Application.java
+++ src/main/java/com/example/demo/EdurekaJenkinsAssign2Application.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EdurekaJenkinsAssign2Application {
-
+public class EdurekaJenkinsAssign2Application extends SpringBootServletInitializer   {
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+	return application.sources(EdurekaJenkinsAssign2Application.class);  
+	}  
 	public static void main(String[] args) {
 		SpringApplication.run(EdurekaJenkinsAssign2Application.class, args);
 	}
